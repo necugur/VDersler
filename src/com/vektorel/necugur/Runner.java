460b2a5916c2b75cb6b4afd2f5ab98bc8a5df07a
necdetozkan <necdetozkan@necdetozkan>
2017-04-10 11:03:07 +0300

    initialize

---
+++ src/com/vektorel/necugur/Runner.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.vektorel.necugur;
+
+/**
+ *
+ * @author necdetozkan
+ */
+public class Runner {
+    
+}
